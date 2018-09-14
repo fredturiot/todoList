94098651a39e12bd79c5327f7894cf7db3f2af34
George Doggo <volty0@orange.fr>
2018-09-14 09:17:13 +0200

    création

---
+++ todoList/Data/TodoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using todoList.Models;
+
+namespace todoList.Data
+{
+	public class TodoDbContext : DbContext
+	{
+		public TodoDbContext() : base("TodoListConnectionString")
+		{
+		}
+
+		public DbSet<Categorie> Categories { get; set; }
+	}
+}
---
+++ todoList/Models/Categorie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace todoList.Models
+{
+	public class Categorie
+	{
+		public int ID { get; set; }
+		
+		public string Nom { get; set; }
+	}
+}
